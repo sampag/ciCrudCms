0c37b5d43315ebb86dc1d5f6dd0afb3e90305b49
sampag <paghunasansam@gmail.com>
2018-01-24 03:07:27 -0800

    404 page

---
+++ application/views/public/page_not_found.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH')OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>404 Error</title>
+	<?php echo link_tag('assets/css/bootstrap.min.css'); ?>
+	<style>
+		body,html{
+			margin-top: 50px;
+		}
+	</style>
+</head>
+<body>
+	 <div class="container">
+      <div class="starter-template text-center">
+        <h1>404 Page not found</h1>
+        <p class="lead text-muted">The page you requested was not found.</p>
+      </div>
+    </div>
+</body>
+</html>
