32ffbda8fe3e6ae51b68d6510324ef1f1c4b1414
sampag <paghunasansam@gmail.com>
2018-01-26 03:36:50 -0800

    Modified

--- application/models/Post_model.php
+++ application/models/Post_model.php
@@ -2,21 +2,114 @@
 defined('BASEPATH')OR exit('No direct script access allowed');
 
 class Post_model extends CI_Model{
-	//=========================================//
-	public function getById($post_id)
+
+	/**
+	* Recent Post for admin
+	*/
+
+	public function recentPost()
+	{	
+		$this->db->limit(10);
+		$this->db->order_by('post_id', 'DESC');
+		$this->db->where('post_trash', NULL);
+		$this->db->join('category', 'category_id = post_category_id', 'left');
+		$query = $this->db->get('post');
+		return $query->result();
+	}
+
+	/**
+	* Meter Widget for admin
+	*/
+	public function meterPost()
 	{
+		$this->db->from('post');
+		$this->db->where('post_trash', NULL);
+		return $this->db->count_all_results();
+	}
+
+	public function meterCategory()
+	{
+		return $this->db->count_all('category');
+	}
+
+	public function meterTag()
+	{
+		return $this->db->count_all('tag');
+	}
+
+	public function meterComment()
+	{
+		return $this->db->count_all('comment');
+	}
+
+	/**
+	* Get single post for admin
+	*/
+
+	public function getByRandomId($random_id)
+	{	
 		$this->db->select('*');
 		$this->db->from('post');
-		$this->db->where('post_id', $post_id);
+		$this->db->where('post_random_id', $random_id);
 		$query = $this->db->get();
 		return $query->row();
 	}
 
-	//=========================================//
-	// All post
+	/**
+	* Restore Post for admin
+	*/
+	public function setRestore($random_id)
+	{
+		$this->db->where('post_random_id', $random_id);
+		$this->db->set('post_trash', NULL);
+		$this->db->update('post');
+	}
+
+	/**
+	* Trash Post admin
+	*/
+	public function getTrash($limit, $start)
+	{
+		$this->db->limit($limit, $start);
+		$this->db->order_by('post_id', 'DESC');
+		$this->db->where('post_trash', TRUE);
+		$this->db->join('category', 'category_id = post_category_id', 'left');
+		$this->db->join('users', 'id = user_id', 'left');
+		$query = $this->db->get('post');	
+
+		if($query->num_rows() > 0 ){
+			foreach($query->result() as $row){
+				$data[] = $row;
+			}
+			return $data;
+		}else{
+			return false;
+		}
+	}
+
+	public function countTrash()
+	{
+		$this->db->from('post');
+		$this->db->where('post_trash', TRUE);
+		return $this->db->count_all_results();
+	}
+
+
+	public function setTrash($random_id)
+	{
+		$this->db->where('post_random_id', $random_id);
+		$this->db->set('post_trash', TRUE);
+		$this->db->update('post');
+	}
+
+
+	/**
+	* Post
+	*/
 	public function getAll($limit, $start)
 	{
 		$this->db->limit($limit, $start);
+		$this->db->where('post_trash', NULL);
 		$this->db->order_by('post_id', 'DESC');
 		$this->db->join('category', 'category_id = post_category_id', 'left');
 		$this->db->join('users', 'id = user_id', 'left');
@@ -34,15 +127,19 @@ class Post_model extends CI_Model{
 
 	public function countAll()
 	{
-		return $this->db->count_all('post');
+		$this->db->from('post');
+		$this->db->where('post_trash', NULL);
+		return $this->db->count_all_results();
 	}
 
-	//=========================================//
-	// Mine post
+	/**
+	* Mine Post
+	*/
 	public function getMine($limit, $start, $user_id)
 	{
 		$this->db->limit($limit, $start);
 		$this->db->where('user_id', $user_id);
+		$this->db->where('post_trash', NULL);
 		$this->db->order_by('post_id', 'DESC');
 		$this->db->join('category', 'category_id = post_category_id', 'left');
 		$query = $this->db->get('post');	
@@ -61,6 +158,7 @@ class Post_model extends CI_Model{
 	{
 		$this->db->from('post');
 		$this->db->where('user_id', $user_id);
+		$this->db->where('post_trash', NULL);
 		return $this->db->count_all_results();
 	}
 
@@ -68,6 +166,7 @@ class Post_model extends CI_Model{
 	{
 		$this->db->limit($limit, $start);
 		$this->db->where('post_published', TRUE);
+		$this->db->where('post_trash', NULL);
 		$this->db->order_by('post_id', 'DESC');
 		$this->db->join('category', 'category_id = post_category_id', 'left');
 		$query = $this->db->get('post');	
@@ -86,6 +185,7 @@ class Post_model extends CI_Model{
 	{
 		$this->db->from('post');
 		$this->db->where('post_published', TRUE);
+		$this->db->where('post_trash', NULL);
 		return $this->db->count_all_results();
 	}
 	
@@ -94,6 +194,7 @@ class Post_model extends CI_Model{
 		$this->db->select('*');
 		$this->db->from('post');
 		$this->db->where('post_random_id', $slug);
+		$this->db->where('post_trash', NULL);
 		$this->db->join('category', 'category.category_id = post.post_category_id', 'left'); // Join
 		$query = $this->db->get();
 		return $query->row();
@@ -150,23 +251,8 @@ class Post_model extends CI_Model{
 	}
 
 
-	public function recent_post()
-	{	
-		$this->db->limit(10);
-		$this->db->order_by('post_id', 'DESC');
-		$this->db->join('category', 'category_id = post_category_id', 'left');
-		$query = $this->db->get('post');
-		return $query->result();
-	}
-
-
 	public function search($limit, $start, $match)
 	{
-		// $this->db->like('post_title', $match);
-		// $this->db->order_by('post_id', 'DESC');
-		// $this->db->join('category', 'category_id = post_category_id', 'left');
-		// $query = $this->db->get('post');
-		// return $query->result();
 
 		$this->db->limit($limit, $start);
 		$this->db->like('post_title', $match);
