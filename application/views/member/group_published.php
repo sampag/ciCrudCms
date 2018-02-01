336913125dae9dce5bc8eaf93c402ad563bb5d62
sampag <paghunasansam@gmail.com>
2018-01-31 23:46:26 -0800

    Modified

--- application/views/member/group_published.php
+++ application/views/member/group_published.php
@@ -3,21 +3,34 @@ defined('BASEPATH')OR exit('No direct script access allowed');
 ?>
 <div class="row">
 	<div class="col-md-12">
+		<?php echo form_open(uri_string().'/trash-multi-post'); ?>
 		<div class="table-responsive">
 			<table class="table table-bordered table-striped">
 				<thead>
 					<tr>
-						<td>Title</td>
-						<td>Categories</td>
-						<td>Tags</td>
-						<td>Created</td>
-						<td class="text-center">Action</td>
+						<th>
+						<?php 
+						  	echo form_submit('postTrash', 'Trash', array('class' => 'btn btn-default btn-xs')); 
+						?>
+						</th>
+						<th>Title</th>
+						<th>Categories</th>
+						<th>Tags</th>
+						<th>Created</th>
+						<th class="text-center">Action</th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php if($item){?>
 					<?php foreach($item as $row){ ?>
 					<tr>
+						<td style="width:20px;">
+							<?php if($row->user_id == $user_id){ ?>
+							<label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect text-center">
+								<input type="checkbox" name="post_trash[]" value="<?php echo $row->post_random_id; ?>" class="mdl-checkbox__input">
+							</label>
+							<?php } ?>
+						</td>
 						<td>
 							<?php
 								$title_limit =  character_limiter($row->post_title, 40);
@@ -27,19 +40,27 @@ defined('BASEPATH')OR exit('No direct script access allowed');
 									echo '<span class="text-muted">'.$title_limit.'</span>';
 								}
 							?>
+							<?php if($row->user_id == $user_id){ ?>
+							<div class="text-sm">
+								<a href="<?php echo base_url('member/post-edit/'.$row->post_random_id); ?>" class="text-muted text-sm po-link">Edit</a> |  
+								<?php
+									echo anchor(uri_string().'/trash/'.$row->post_random_id, 'Trash', array('class' => 'text-sm text-muted po-link'));
+								?>
+							</div>
+							<?php } ?>
 						</td>
 						<td class="list-category">
 							<?php 
 								if($row->user_id == $user_id)
 								{
-									if($row->post_category_id == '0'){
+									if($row->post_category_id == 0){
 										echo anchor('member/post/'.$row->post_uncategorized_slug, 'Uncategorized', array('class'=>'post-list'));
 
 									}else{
 										echo anchor('member/post-category/'. $row->category_slug, $row->category_name, array('class'=>'post-list'));
 									}
 								}else{
-									if($row->post_category_id == '0'){
+									if($row->post_category_id == 0){
 										echo '<span class="text-muted">Uncategorized</span>';
 									}else{
 										echo '<span class="text-muted">'.$row->category_name.'</span>';
@@ -71,12 +92,12 @@ defined('BASEPATH')OR exit('No direct script access allowed');
 								}						
 							?>
 						</td>
-						<td class="list-category">
+						<td class="list-category text-muted">
 							<?php 
 								if($row->post_published_created){
-									echo '<span class="text-muted">'. date('m/d/Y', strtotime($row->post_published_created)).'</span>';
+									echo date('M d, Y', strtotime($row->post_published_created));
 								}
-							 ?>
+							?>
 						</td>
 						<td class="text-center list-category">
 							<?php
@@ -96,15 +117,21 @@ defined('BASEPATH')OR exit('No direct script access allowed');
 				</tbody>
 				<thead>
 					<tr>
-						<td>Title</td>
-						<td>Categories</td>
-						<td>Tags</td>
-						<td>Created</td>
-						<td class="text-center">Action</td>
+						<th>
+						<?php 
+						  	echo form_submit('postTrash', 'Trash', array('class' => 'btn btn-default btn-xs')); 
+						?>
+						</th>
+						<th>Title</th>
+						<th>Categories</th>
+						<th>Tags</th>
+						<th>Created</th>
+						<th class="text-center">Action</th>
 					</tr>
 				</thead>
 			</table>
 		</div>
+		<?php echo form_close(); ?>
 	</div>
 </div><!-- row -->
 <div class="row">
