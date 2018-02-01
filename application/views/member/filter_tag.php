7ef445d80513ebf800746608b38bcb3b69047d55
sampag <paghunasansam@gmail.com>
2018-01-31 23:51:46 -0800

    Modified

--- application/views/member/filter_tag.php
+++ application/views/member/filter_tag.php
@@ -35,14 +35,20 @@ defined('BASEPATH')OR exit('No direct script access allowed');
 <br>
 <div class="row">
 	<div class="col-md-12">
+		<?php echo form_open(uri_string().'/trash-multi-post'); ?>
 		<div class="table-responsive">
 			<table class="table table-hover table-bordered table-striped">
 				<thead>
 					<tr>
-						<td>Title</td>
-						<td>Category</td>
-						<td>Tag</td>
-						<td>Created</td>
+						<th>
+						<?php 
+						  	echo form_submit('postTrash', 'Trash', array('class' => 'btn btn-default btn-xs')); 
+						?>
+						</th>
+						<th>Title</th>
+						<th>Categories</th>
+						<th>Tags</th>
+						<th>Created</th>
 					</tr>
 				</thead>
 					<tbody>
@@ -51,6 +57,11 @@ defined('BASEPATH')OR exit('No direct script access allowed');
 						foreach($post as $row): 
 					?>
 					<tr>
+						<td style="width:20px;">
+							<label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect text-center">
+								<input type="checkbox" name="post_trash[]" value="<?php echo $row->post_random_id; ?>" class="mdl-checkbox__input">
+							</label>
+						</td>
 						<td class="list-title">	
 							<div class="po-markup">
 								<?php 
@@ -83,6 +94,12 @@ defined('BASEPATH')OR exit('No direct script access allowed');
 									</div>
 								</div>
 							</div>
+							<div class="text-sm">
+								<a href="<?php echo base_url('member/post-edit/'.$row->post_random_id); ?>" class="text-muted text-sm po-link">Edit</a> |  
+								<?php
+									echo anchor(uri_string().'/trash/'.$row->post_random_id, 'Trash', array('class' => 'text-sm text-muted po-link'));
+								?>
+							</div>
 							</td>
 						<td class="list-category">
 						<?php
@@ -108,9 +125,11 @@ defined('BASEPATH')OR exit('No direct script access allowed');
 									endforeach;
 								?>
 						</td>
-						<td class="list-tag">
+						<td class="list-tag text-muted">
 							<?php 
-							echo date('m/d/Y', strtotime($row->post_published_created));
+								if($row->post_published_created){
+									echo date('M d, Y', strtotime($row->post_published_created));
+								}
 							?>
 						</td>
 					</tr>
@@ -122,14 +141,20 @@ defined('BASEPATH')OR exit('No direct script access allowed');
 				</tbody>
 				<thead>
 					<tr>
-						<td>Title</td>
-						<td>Category</td>
-						<td>Tag</td>
-						<td>Created</td>
+						<th>
+						<?php 
+						  	echo form_submit('postTrash', 'Trash', array('class' => 'btn btn-default btn-xs')); 
+						?>
+						</th>
+						<th>Title</th>
+						<th>Categories</th>
+						<th>Tags</th>
+						<th>Created</th>
 					</tr>
 				</thead>
 			</table>
 		</div>
+		<?php echo form_close(); ?>
 	</div>
 </div>
 	<div class="row">
