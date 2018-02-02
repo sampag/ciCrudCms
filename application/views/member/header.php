af6a3a846a10feccf18690874fe3cc83e9b0ddd6
sampag <paghunasansam@gmail.com>
2018-02-02 04:06:10 -0800

    Modified

--- application/views/member/header.php
+++ application/views/member/header.php
@@ -132,6 +132,9 @@ defined('BASEPATH')OR exit('No direct script access allowed');
         .select-all{
             padding-left: 9px;
         }
+        .text-flat-success{
+            color: #37c55e;
+        }
     </style>
 </head>
 <body>
@@ -147,29 +150,6 @@ defined('BASEPATH')OR exit('No direct script access allowed');
                 <?php echo anchor('member', $site_title, array('class' => 'navbar-brand')); ?>
             </div>
             <ul class="nav navbar-right top-nav">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-comment"></i></a>
-                    <ul class="dropdown-menu message-dropdown">
-                        <li class="message-preview">
-                            <a href="#">
-                                <div class="media">
-                                    <span class="pull-left">
-                                        <img class="media-object" src="#" alt="">
-                                    </span>
-                                    <div class="media-body">
-                                        <h5 class="media-heading"><strong>John Smith</strong>
-                                        </h5>
-                                        <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-                                        <p>Lorem ipsum dolor sit amet, consectetur...</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="message-footer">
-                            <a href="#">Read All New Messages</a>
-                        </li>
-                    </ul>
-                </li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bell"></i></a>
                     <ul class="dropdown-menu alert-dropdown">
